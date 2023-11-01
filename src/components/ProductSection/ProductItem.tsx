6bc6712efb8aa43322408489daf16bad14bff221
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-11-01 00:29:37 -0300

    Button to next/link in productItem

--- src/components/ProductSection/ProductItem.tsx
+++ src/components/ProductSection/ProductItem.tsx
@@ -1,11 +1,16 @@
 import { Product } from "@prisma/client"
+import { Flex } from "@radix-ui/themes"
 import Image from "next/image"
+import Link from "next/link"
 
 function ProductItem({ product }: { product: Product }) {
   return (
-    <li className='w-[230px] bg-red-green-400  max-sm:mx-auto'>
-      <button className='text-left rounded-xl border-2 border-gray-200 transition-all hover:border-primary hover:border-2 hover:scale-110'>
-        <div className='bg-offwhite rounded-t-xl'>
+    <li className='w-[230px] shadow max-sm:mx-auto hover:scale-105 transition-all'>
+      <Link
+        className='text-left rounded-xl'
+        href={`/product/${product.productId}`}
+      >
+        <div className='bg-offwhite rounded-t'>
           <Image
             src={product.images[0]}
             width={550}
@@ -13,13 +18,22 @@ function ProductItem({ product }: { product: Product }) {
             alt={`${product.name} image`}
           />
         </div>
-        <div className='bg-white h-[40px] px-3 rounded-b-xl mt-2'>
-          <p className='font-medium'>{product.name}</p>
+        <div className='bg-white h-[30px] px-3 rounded-b mt-2'>
+          <p className='text-sm font-medium my-auto text-gray-600'>
+            {product.name}
+          </p>
         </div>
-        <div className='p-2 mt-3'>
-          <span className='font-bold text-green-600'>$ {product.price}</span>
+        <div className='p-2 mt-1'>
+          <Flex justify='between' align='center'>
+            <span className='font-medium text-2xl'>${product.price}</span>
+            <div>
+              <span className='self-end text-sm text-primary font-medium underline'>
+                Visit
+              </span>
+            </div>
+          </Flex>
         </div>
-      </button>
+      </Link>
     </li>
   )
 }
