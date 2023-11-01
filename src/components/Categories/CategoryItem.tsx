2b67b9c102555709c32586bc891554a223abbc01
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-11-01 00:31:09 -0300

    Correct redirect when category is clicked

--- src/components/Categories/CategoryItem.tsx
+++ src/components/Categories/CategoryItem.tsx
@@ -5,7 +5,7 @@ import { ChevronRight } from "lucide-react"
 
 function CategoryItem({ category }: { category: Category }) {
   return (
-    <Link href='#'>
+    <Link href={`/category/${category.categoryId}`}>
       <Box className='py-1 px-3 bg-secondary hover:bg-primary hover:text-white transition-colors rounded-2xl'>
         <Flex gap='1' align='center' justify='between'>
           <Text size='2' weight='medium'>
