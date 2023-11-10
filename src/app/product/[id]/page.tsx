709ab091af8315e822380688b259040591eb39e7
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-11-09 22:03:32 -0300

    Single product view

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,47 +2,61 @@
 
 import { useCases } from "@/api/useCases"
 import { Product } from "@prisma/client"
-import { Box, Badge, Flex, Button } from "@radix-ui/themes"
+import { Box, Badge, Button } from "@radix-ui/themes"
 import { useParams } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import Image from "next/image"
 import productDetailStyles from "@/styles/product-detail.module.css"
-import { ShoppingCart } from "lucide-react"
+import { Minus, Plus, ShoppingCart } from "lucide-react"
+import { useCartStore } from "@/store/useCartStore"
+import Link from "next/link"
 
-const PRODUCT = {
-  productId: "653c433ba2181dc2e5da2920",
-  name: "Samsung A34 6GB RAM|128GB",
-  productCategoryId: "653c42b7a2181dc2e5da291d",
-  price: 150,
-  description:
-    "Samsung Galaxy A34 phone with 5G technology with 6gb RAM and 128GB of Storage",
-  images: ["http://localhost:3000/SA34.png"],
-  features: [
-    "6GB RAM",
-    "128GB Storage",
-    "Camera 13mp",
-    "Color: Awesome Silver",
-  ],
+const INITIAL_STATE = {
+  productId: "",
+  name: "",
+  productCategoryId: "",
+  price: 0,
+  description: "",
+  images: [],
+  features: [],
   category: {
-    categoryId: "653c42b7a2181dc2e5da291d",
-    name: "Phone",
+    categoryId: "",
+    name: "",
   },
 }
 
 function ProductByID() {
   const { id } = useParams()
 
-  const [product, setProduct] = useState<Product>(PRODUCT)
-  // const [isLoading, setisLoading] = useState(false)
-  /* useEffect(() => {
+  const [product, setProduct] = useState<Product>(INITIAL_STATE)
+  const { addItem, cartItems, removeItem, setCartItems } = useCartStore()
+  const [isLoading, setisLoading] = useState(true)
+  useEffect(() => {
     const getProduct = async () => {
+      setisLoading(true)
       const product = await useCases.products.getById(id as string)
       setProduct(product.data.data[0])
+      setisLoading(false)
     }
     getProduct()
-  }, [id]) */
+  }, [id, setCartItems])
 
-  return (
+  const isAlreadyOnTheCart = useMemo(
+    () => cartItems.find((e) => e.productId === product.productId),
+    [cartItems, product.productId]
+  )
+  const handleClickCartButton = () => {
+    if (isAlreadyOnTheCart) {
+      console.log("is on the cart... removing")
+      return removeItem(product)
+    }
+    addItem(product)
+    window.localStorage.setItem("cart", JSON.stringify(cartItems))
+  }
+
+  return isLoading ? (
+    <div>cargando...</div>
+  ) : (
     <div
       className={`${productDetailStyles["product-detail"]} mt-8 bg-secondary rounded-md max-w-5xl mx-auto`}
     >
@@ -55,22 +69,32 @@ function ProductByID() {
           alt={`${product.name} preview image`}
         />
       </Box>
-      <Box className='p-8 flex border-l justify-center border-gray-300 mx-auto'>
+      <Box className='p-8 flex border-l justify-center max-md:border-none border-gray-300 mx-auto'>
         <div className='flex-col gap-2'>
           <h2 className='text-3xl font-semibold'>{product.name}</h2>
           <p className='text-3xl font-bold my-2'>${product.price}</p>
           <Badge color='blue' size='2'>
-            {product.category.name}
+            <Link href={`/category/${product.category.categoryId}`}>
+              {product.category.name}
+            </Link>
           </Badge>
-          <ul className='list-disc my-2 text-sm mt-8'>
-            {product.features.map((feature, i) => (
-              <li key={`${feature}-${i}`}>{feature}</li>
-            ))}
-          </ul>
+          <section className='mt-4'>
+            <ul className='list-disc text-sm'>
+              {product.features.map((feature, i) => (
+                <li key={`${feature}-${i}`}>{feature}</li>
+              ))}
+            </ul>
+          </section>
           <div className='flex justify-center mt-4'>
-            <Button  size='4' >
+            <Button
+              size='4'
+              style={{ cursor: "pointer" }}
+              onClick={handleClickCartButton}
+            >
               <ShoppingCart />
-              <span>Add to cart</span>
+              <span>
+                {isAlreadyOnTheCart ? "Remove from cart" : "Add to cart"}
+              </span>
             </Button>
           </div>
         </div>
