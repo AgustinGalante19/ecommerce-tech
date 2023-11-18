0f23f8a1bce9e658f062956d83ddc102d35baebd
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-11-18 20:53:51 -0300

    CategoryList as client component

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,29 +1,24 @@
+"use client"
+
 import { Container, Flex } from "@radix-ui/themes"
 import CategoryItem from "./CategoryItem"
+import { useEffect, useState } from "react"
 import { Category } from "@prisma/client"
-import { API_URL } from "@/libs/API"
+import api from "@/api"
 
-async function getCategories() {
-  try {
-    const API = `${API_URL}/category`
-    console.log("API URL: ", API)
-    const request = await fetch(API)
-    const categories: ApiResponse<Category> = await request.json()
-    return categories
-  } catch (err) {
-    return {
-      data: [],
-    }
-  }
-}
+function Categories() {
+  const [categories, setCategories] = useState<Category[]>([])
+  useEffect(() => {
+    api
+      .get<ApiResponse<Category>>("/category")
+      .then((response) => setCategories(response.data.data))
+  }, [])
 
-async function Categories() {
-  const categoryList = await getCategories()
   return (
     <div className='py-4 border-b border-[#EDEDED]'>
       <Container>
         <Flex gap='4' wrap='wrap'>
-          {categoryList.data.map((cat) => (
+          {categories.map((cat) => (
             <CategoryItem category={cat} key={cat.categoryId} />
           ))}
         </Flex>
