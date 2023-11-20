1aadedb312356fe75ef2ce8161d9984b2fd6fc20
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-11-20 16:49:14 -0300

    using promise instead async-await

--- src/hooks/useProductsByCatID.ts
+++ src/hooks/useProductsByCatID.ts
@@ -9,11 +9,12 @@ export default function useProductByCatID({ id }: { id: string }) {
 
   useEffect(() => {
     const getProducts = async () => {
-      setIsLoading(true)
-      const products = await useCases.products.getByCatId(id as string)
-      setCategoryName(products.data.data[0].category.name)
-      setProductsData(products.data.data)
-      setIsLoading(false)
+      useCases.products.getByCatId(id as string).then((response) => {
+        const productsList = response.data.data
+        setProductsData(productsList)
+        setCategoryName(productsList[0].category.name)
+        setIsLoading(false)
+      })
     }
     getProducts()
   }, [id])
