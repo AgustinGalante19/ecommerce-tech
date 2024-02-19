d9e6d3b3e5355f969822c9ca5fba8b7ef9fe171a
Agustin Galante <agustin.galante.19@outlook.es>
2024-02-19 11:49:10 -0300

    Test error deploy

--- src/app/api/product/initialData/route.ts
+++ src/app/api/product/initialData/route.ts
@@ -4,30 +4,39 @@ import TResponse from "@/types/InitialDataResponse"
 import { NextResponse } from "next/server"
 
 export async function GET(): Promise<NextResponse<ApiResponse<TResponse>>> {
-  const categories = await client.category.findMany()
+  try {
+    const categories = await client.category.findMany()
 
-  const length = categories.length
-  const indexes = randomIndex([length, length])
+    const length = categories.length
+    const indexes = randomIndex([length, length])
 
-  const promises = indexes.map((randomIndex) => {
-    const catId = categories[randomIndex].categoryId
-    return client.product.findMany({
-      where: {
-        productCategoryId: catId,
-      },
-      include: {
-        category: true,
-      },
+    const promises = indexes.map((randomIndex) => {
+      const catId = categories[randomIndex].categoryId
+      return client.product.findMany({
+        where: {
+          productCategoryId: catId,
+        },
+        include: {
+          category: true,
+        },
+      })
     })
-  })
 
-  const result = await Promise.all(promises)
+    const result = await Promise.all(promises)
 
-  const data = result.map((item) => ({
-    catId: item.at(0)?.category.categoryId ?? "",
-    category: item.at(0)?.category.name ?? "",
-    products: item,
-  }))
+    const data = result.map((item) => ({
+      catId: item.at(0)?.category.categoryId ?? "",
+      category: item.at(0)?.category.name ?? "",
+      products: item,
+    }))
 
-  return NextResponse.json({ data, error: [], result: "ok" })
+    return NextResponse.json({ data, error: [], result: "ok" })
+  } catch (err) {
+    console.log("error intial data endpoint")
+    return NextResponse.json({
+      data: [],
+      error: ["Unexpected error"],
+      result: "error",
+    })
+  }
 }
