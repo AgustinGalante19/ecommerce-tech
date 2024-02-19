b60db756a653ddb21d459c78d33024f8fca89bd9
Agustin Galante <agustin.galante.19@outlook.es>
2024-02-19 12:18:53 -0300

    debug log

--- src/actions/product.ts
+++ src/actions/product.ts
@@ -4,32 +4,42 @@ import client from "@/libs/prisma"
 import randomIndex from "@/libs/randomIndex"
 
 export async function getInitialData() {
-  const categories = await client.category.findMany()
+  try {
+    const categories = await client.category.findMany()
 
-  const length = categories.length
-  const indexes = randomIndex([length, length])
+    const length = categories.length
+    const indexes = randomIndex([length, length])
 
-  const promises = indexes.map((randomIndex) => {
-    const catId = categories[randomIndex].categoryId
-    return client.product.findMany({
-      where: {
-        productCategoryId: catId,
-      },
-      include: {
-        category: true,
-      },
+    const promises = indexes.map((randomIndex) => {
+      const catId = categories[randomIndex].categoryId
+      return client.product.findMany({
+        where: {
+          productCategoryId: catId,
+        },
+        include: {
+          category: true,
+        },
+      })
     })
-  })
 
-  const result = await Promise.all(promises)
+    const result = await Promise.all(promises)
 
-  const data = result.map((item) => ({
-    catId: item.at(0)?.category.categoryId ?? "",
-    category: item.at(0)?.category.name ?? "",
-    products: item,
-  }))
+    const data = result.map((item) => ({
+      catId: item.at(0)?.category.categoryId ?? "",
+      category: item.at(0)?.category.name ?? "",
+      products: item,
+    }))
 
-  return { products: data, categories, error: [], result: "ok" }
+    return { products: data, categories, error: [], result: "ok" }
+  } catch (err) {
+    console.log("error initial data: ", err)
+    return {
+      products: [],
+      categories: [],
+      error: ["Unexpected error"],
+      result: "error",
+    }
+  }
 }
 
 export async function getProductById(id: string) {
